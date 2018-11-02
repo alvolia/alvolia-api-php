ae7655b7d990585bfeaa87904e793c0df6c6f8d7
Matej Kloska <matej.kloska@gmail.com>
2018-11-02 09:27:26 +0100

    Refactor code so no duplicit initialization of CURL is made

--- src/Client.php
+++ src/Client.php
@@ -19,17 +19,23 @@ class Client {
     }
 
     public function post($endpoint, $data) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->endpointURL($endpoint));
+        $ch = $this->initializeCURLHandle(
+            $this->endpointURL($endpoint)
+        );
+
+        // options for HTTP POST request
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->defaultHeaders());
-
+        // fire request
         $response_data = curl_exec($ch);
+
+        // check if any error occured during request
         $err_code = curl_errno($ch);
+
+        // do not forget to free memory
         curl_close ($ch);
+
         if ($err_code == 0) {
             return json_decode($response_data, false);
         } else {
@@ -38,16 +44,19 @@ class Client {
     }
 
     public function get($endpoint) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->endpointURL($endpoint));
+        $ch = $this->initializeCURLHandle(
+            $this->endpointURL($endpoint)
+        );
 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->defaultHeaders());
-
+        // fire request
         $response_data = curl_exec($ch);
+
+        // check error code
         $err_code = curl_errno($ch);
+
+        // free memory
         curl_close ($ch);
+
         if ($err_code == 0) {
             return json_decode($response_data, false);
         } else {
@@ -57,13 +66,36 @@ class Client {
 
     protected function defaultHeaders() {
         return array(
+            // required to identifie site for API requests
             'X-Site: ' . $this->_clientID,
+            // token identifies who is accessing data
             'X-Token: ' . $this->_token,
+            // API requires this content type
             "Content-Type: application/x-www-form-urlencoded",
         );
     }
 
     protected function endpointURL($endpoint) {
         return self::API_ROOT . '/' . self::API_VERSION . '/' . ltrim($endpoint, '/');
+    }
+
+    protected function initializeCURLHandle($url) {
+        $ch = curl_init();
+
+        // URL is required of any request, so set it at the beginning of initialization
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        // just to be sure we do not block interface for a long time
+        // our response should be made within a 25ms, so 1s is more than enough
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+
+        // always wait for reponse from server and return it
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // headers are always the same - we need to send auth identifier and token
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->defaultHeaders());
+
+        return $ch;
     }
 }
